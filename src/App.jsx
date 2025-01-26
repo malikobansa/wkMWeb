069532637b8e829edf8c3eec2d50d7b0ed589712
malikobansa <hackobansa@gmail.com>
2025-01-26 22:34:54 +0100

    added animated-cursor to replace main cursor

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,27 @@
+import AnimatedCursor from 'react-animated-cursor'
 import Layout from './components/layouts/Layout'
 
 const App = () => {
   return (
+    <>
+      <AnimatedCursor
+        innerSize={8}
+        outerSize={35}
+        color="white"
+        outerAlpha={0.4}
+        innerScale={0.7}
+        outerScale={1.5}
+        outerStyle={{
+          border: '3px solid black'
+        }}
+        innerStyle={{
+          backgroundColor: 'white'
+        }}
+      />
     <Layout>
-      <div>App</div>
+      <AnimatedCursor/>
     </Layout>
+    </>
   )
 }
 
